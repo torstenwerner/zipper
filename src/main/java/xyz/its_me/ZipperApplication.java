c8e206733afbd79bd554f0a68650e9c2b2ccc193
Torsten Werner <torsten.werner@westernacher.com>
2015-12-17 22:25:23 +0100

    implement main

--- src/main/java/xyz/its_me/ZipperApplication.java
+++ src/main/java/xyz/its_me/ZipperApplication.java
@@ -1,12 +1,26 @@
 package xyz.its_me;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.*;
 
-@SpringBootApplication
 public class ZipperApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(ZipperApplication.class, args);
+        zip("src", "build/src.zip");
+    }
+
+    private static void zip(String rootFolder, String target) {
+        try (final ZipCollector collector = new ZipCollector(new FileOutputStream(target))) {
+            new FilesystemFetcher().iterate(new File(rootFolder))
+                    .forEach((path, file) -> collector.add(path, file2Stream(file)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static InputStream file2Stream(File file) {
+        try {
+            return new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("failed to read file " + file.getName());
+        }
     }
 }
